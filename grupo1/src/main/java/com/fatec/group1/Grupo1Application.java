638c070ba4acc5843964171b7da65645eb951d90
dti <dti@LAB204-19>
2022-09-06 11:33:43 -0300

    autenticacao, conexao com o banco, criacao da tela de login

--- grupo1/src/main/java/com/fatec/group1/Grupo1Application.java
+++ grupo1/src/main/java/com/fatec/group1/Grupo1Application.java
@@ -2,12 +2,14 @@ package com.fatec.group1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class Grupo1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo1Application.class, args);
+		System.out.println(new BCryptPasswordEncoder().encode("123"));
 	}
 
 }
