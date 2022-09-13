6e4607619968f736439e5d1ec668dc9387de4b12
dti <dti@LAB204-19>
2022-09-13 11:17:14 -0300

    user e senha para heroku

--- grupo1/src/main/java/com/fatec/group1/Grupo1Application.java
+++ grupo1/src/main/java/com/fatec/group1/Grupo1Application.java
@@ -1,16 +1,35 @@
 package com.fatec.group1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder; 
 
+import com.fatec.group1.model.UserAppRepository;
+import com.fatec.group1.model.UsuarioApp; 
 @SpringBootApplication
 public class Grupo1Application {
-
+	@Autowired
+	 PasswordEncoder passwordEncoder;
+	 @Autowired
+	 UserAppRepository repository;
 	public static void main(String[] args) {
 		SpringApplication.run(Grupo1Application.class, args);
 		System.out.println(new BCryptPasswordEncoder().encode("123"));
 		System.out.println(new BCryptPasswordEncoder().encode("456"));
+	}
+	
+	@Autowired
+	public void inicializa() {
+		UsuarioApp usuario = new UsuarioApp();
+		usuario.setUserName("jose");
+		usuario.setPassword(passwordEncoder.encode("123"));
+		repository.save(usuario);
+	}
+
+	public BCryptPasswordEncoder pc() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
