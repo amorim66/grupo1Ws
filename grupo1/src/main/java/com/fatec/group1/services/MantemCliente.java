1ab6f1776b20b50f98c6994f38c817640a3f9483
dti <dti@LAB204-19>
2022-09-20 10:54:03 -0300

    classe cliente, cadastro de cliente

---
+++ grupo1/src/main/java/com/fatec/group1/services/MantemCliente.java
@@ -0,0 +1,21 @@
+package com.fatec.group1.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.fatec.group1.model.Cliente;
+
+
+public interface MantemCliente {
+	List<Cliente> consultaTodos();
+
+	Optional<Cliente> consultaPorCpf(String cpf);
+
+	Optional<Cliente> consultaPorId(Long id);
+
+	Optional<Cliente> save(Cliente cliente);
+
+	void delete(Long id);
+
+	Optional<Cliente> atualiza(Cliente cliente);
+}
